23f70fb8ad7cf6e555fa0f314dbf15b044abb084
Андрей <parabellum.koval@gmail.com>
2023-03-21 20:38:16 +0200

    refferals for socialite

--- server/app/Http/Controllers/Auth/OAuthController.php
+++ server/app/Http/Controllers/Auth/OAuthController.php
@@ -65,7 +65,15 @@ class OAuthController extends Controller
     
     $user_data = Socialite::driver($service)->stateless()->user();
     
-    \Log::info(print_r($user_data, true));
+    // parse referrer code
+    $state = $request->input('state');
+
+    if(!empty($state)){
+      parse_str($state, $result);
+      if(isset($result['referrer_code']) && !empty($result['referrer_code'])) {
+        $referrer = Profile::where('referrer_code', $result['referrer_code'])->first();
+      } 
+    }
 
     $fullname = explode(' ', $user_data->getName());
     $email = $user_data->getEmail();
@@ -83,6 +91,7 @@ class OAuthController extends Controller
         'photo' => $user_data->getAvatar(),
         'password' => Hash::make($password),
         'email_verified_at' => now(),
+        'referrer_id' => isset($referrer) && $referrer? $referrer->id: null,
         'referrer_code' => Str::random(8)
       ]
     );
@@ -93,8 +102,9 @@ class OAuthController extends Controller
     return redirect(env('FRONT_URL', 'http://localhost:3000') . '?token=' . $token->plainTextToken);
   }
 
-  public function redirect($service) {   
-    $url = Socialite::driver($service)->redirect()->getTargetUrl();
+  public function redirect(Request $request, $service) {   
+    $referrer_code = isset($request->referrer_code)? $request->referrer_code: null;
+    $url = Socialite::driver($service)->with(['state' => "referrer_code={$referrer_code}"])->redirect()->getTargetUrl();
     return $url;
   }
 }
